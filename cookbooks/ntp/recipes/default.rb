13212549ff5fda711759238315da309c320a5c1d
sanjay pandrala <sanjay3902@gmail.com>
2017-07-04 17:03:35 -0400

    few ntp and mysql

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -1,22 +1,4 @@
-#
-# Cookbook:: ntp
-# Recipe:: default
-# Author:: Joshua Timberman (<joshua@chef.io>)
-# Author:: Tim Smith (<tsmith@chef.io>)
-#
-# Copyright:: 2009-2017, Chef Software, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+###############
 
 ::Chef::Resource.send(:include, Opscode::Ntp::Helper)
 
@@ -136,4 +118,4 @@ service node['ntp']['service'] do
   timeout 120 if platform_family?('windows')
   retries 3
   retry_delay 5
-end
+en
